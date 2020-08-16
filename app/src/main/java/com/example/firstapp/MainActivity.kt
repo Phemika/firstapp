7aa15846927fcf8c76a54f383f748782588059b2
a <pamika.si@rmuti.ac.th>
2020-08-16 23:54:14 +0700

    Create newwordactivity,word,worddao,wordlistadapter,wordrepository,wordroomdatabase,wordviewmodel

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +22,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
---
+++ app/src/main/java/com/example/firstapp/WordDao.kt
@@ -0,0 +1,24 @@
+package com.example.firstapp
+
+import android.arch.lifecycle.LiveData
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
+
+@Dao
+interface WordDao {
+
+    // LiveData is a data holder class that can be observed within a given lifecycle.
+    // Always holds/caches latest version of data. Notifies its active observers when the
+    // data has changed. Since we are getting all the contents of the database,
+    // we are notified whenever any of the database contents have changed.
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    fun getAlphabetizedWords(): LiveData<List<Word>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    fun deleteAll()
+}
